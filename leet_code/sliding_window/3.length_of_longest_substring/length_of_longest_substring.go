5640d84e290287efb3e0b6eed6981cc2a9e47d08
lihaoran <bill_lhr@sina.com>
2022-08-01 10:13:09 +0800

    lcs

--- leet_code/sliding_window/3.length_of_longest_substring/length_of_longest_substring.go
+++ leet_code/sliding_window/3.length_of_longest_substring/length_of_longest_substring.go
@@ -1,15 +1,19 @@
 /**
  * @Author: lihaoran
  * @Date: 2022/7/25 23:35
- * @Description:
  */
 
 package length_of_longest_substring
 
-// https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+/*
+ * @Description:
+	3. 无重复字符的最长子串
+	给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+	链接：https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+*/
 func lengthOfLongestSubstring(s string) int {
-	left, right := 0, 0
 	maxLen := 0
+	left, right := 0, 0
 	m := make(map[byte]int)
 
 	for right < len(s) {
---
+++ leet_code/sliding_window/3.length_of_longest_substring/length_of_longest_substring_test.go
@@ -0,0 +1,10 @@
+package length_of_longest_substring
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_lengthOfLongestSubstring(t *testing.T) {
+	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
+}
